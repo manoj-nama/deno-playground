9475b1a91c24bba3352662534cbd8c9819bfd1c2
Manoj Nama <manoj.nama@outlook.com>
2020-05-17 21:47:52 +1000

    Adding postgres module, adding abiility to connect to existing database

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+import config from "./config.ts";
+
+export const query = async (dbQuery: string, args: any = []) => {
+  const client = new Client((config as any).database);
+  try {
+    await client.connect();
+  } catch (e) {
+    console.log("Connection error", e);
+  }
+  const result = await client.query({
+    text: dbQuery,
+    args
+  });
+  await client.end();
+  return result.rowsOfObjects();
+};
